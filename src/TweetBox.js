ea9192ea13d8fcc5ba0c14ef7a2c6eaa66a7076c
Parth Brahmaxatri <parthbrahmaxatri007@gmail.com>
2021-06-10 00:18:38 +0530

    :card_file_box: add ability to post a tweet to the database

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import "./TweetBox.css";
-import {Button} from "@material-ui/core";
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {Avatar, Button} from "@material-ui/core";
+import db from "./firebase";
 
 function TweetBox() {
+    const [tweetMessage, setTweetMessage] = useState("");
+
+    const sendTweet = (e) => {
+        e.preventDefault();
+
+        db.collection("posts").add({
+            displayName: "Anonymous User",
+            username: "anon",
+            verified: false,
+            text: tweetMessage,
+        });
+
+        setTweetMessage("");
+    }
+
     return (
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
-                    <AccountCircleIcon></AccountCircleIcon>
-                    <input placeholder="What's happening?" type="text"></input>
+                    <Avatar />
+                    <input
+                        onChange={(e) => setTweetMessage(e.target.value)}
+                        value={tweetMessage}
+                        placeholder="What's happening?"
+                        type="text"
+                    />
                 </div>
-                <Button className="tweetBox__tweetButton" >Tweet</Button>
+                <Button
+                    onClick={sendTweet}
+                    type="submit"
+                    className="tweetBox__tweetButton"
+                >
+                    Tweet
+                </Button>
             </form>
         </div>
-    )
+    );
 }
 
 export default TweetBox;
